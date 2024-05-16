fc534b5cf420c155e3cbc193b867375bed711602
Jeremy Frank <jeremyjfrank@gmail.com>
2024-05-16 09:09:56 -0400

    Add pages collection

---
+++ apps/cms/src/collections/Pages.ts
@@ -0,0 +1,34 @@
+import type { CollectionConfig } from 'payload/types'
+
+import { slugField } from '../fields/slug'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  access: {
+    read: () => true,
+  },
+  admin: {
+    defaultColumns: ['title', 'slug', 'postDate'],
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    slugField(),
+    {
+      name: 'postDate',
+      type: 'date',
+      required: true,
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+        },
+        position: 'sidebar',
+      },
+      defaultValue: () => new Date(),
+    },
+  ],
+}
