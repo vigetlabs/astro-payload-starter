e88ef0873265ed5629fb04a03cad56a01d9ec690
Jeremy Frank <jeremyjfrank@gmail.com>
2024-05-21 13:42:23 -0400

    Fix __dirname not defined in ES module scope error

--- apps/cms/src/collections/Media.ts
+++ apps/cms/src/collections/Media.ts
@@ -1,5 +1,9 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import type { CollectionConfig } from 'payload/types'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
