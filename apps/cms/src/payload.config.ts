e7298e18c15edcce424f2f59b2c70c70217e3cfd
Jeremy Frank <jeremyjfrank@gmail.com>
2024-05-22 10:52:11 -0400

    Remove payload plugin cloud and update deps

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -1,5 +1,4 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
-// import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload/config'
@@ -26,7 +25,6 @@ export default buildConfig({
   collections: [Categories, Media, Pages, Posts, ReusableContent, Users],
   globals: [Footer, MainMenu],
   editor: lexicalEditor({}),
-  // plugins: [payloadCloud()], // TODO: Re-enable when cloud supports 3.0
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
