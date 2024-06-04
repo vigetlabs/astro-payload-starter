36f9f7df55c041ae4300b2c95da156684ec532e7
Jeremy Frank <jeremyjfrank@gmail.com>
2024-06-04 16:32:40 -0400

    Add site option to Astro config

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import 'dotenv/config'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.SITE_URL || 'https://site.com/',
+})
