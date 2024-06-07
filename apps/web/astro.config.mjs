4f5114ce6a3f807fdc702cedc44b88e5b2b59b0a
Jeremy Frank <jeremyjfrank@gmail.com>
2024-06-07 11:29:42 -0400

    Add localhost to authorized domains for images

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ['localhost'],
+  },
   site: process.env.SITE_URL || 'https://site.com/',
 })
