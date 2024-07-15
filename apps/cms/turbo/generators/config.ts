618c7e3ae2333500c9da2d2a9d290f67db52831d
Jeremy Frank <jeremyjfrank@gmail.com>
2024-07-15 16:39:10 -0400

    Extends cms generator to import collection config

--- apps/cms/turbo/generators/config.ts
+++ apps/cms/turbo/generators/config.ts
@@ -19,12 +19,38 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
         message: 'What is the name of the configuration?',
       },
     ],
-    actions: [
-      {
+    actions: (data) => {
+      const actions: any[] = []
+
+      // Add the configuration file
+      actions.push({
         type: 'add',
         path: 'src/{{lowerCase type}}s/{{pascalCase name}}.ts',
         templateFile: 'templates/{{lowerCase type}}.hbs',
-      },
-    ],
+      })
+
+      // Conditionally add collection configurations to the Payload config
+      if (data.type === 'Collection') {
+        // First import the collection configuration
+        actions.push({
+          type: 'append',
+          path: 'src/payload.config.ts',
+          pattern: /from '\.\/collections\/Users'(?<insertion>)/g,
+          template:
+            "import { {{pascalCase name}} } from './collections/{{pascalCase name}}'",
+        })
+
+        // Then add it to the collections array
+        actions.push({
+          type: 'append',
+          path: 'src/payload.config.ts',
+          pattern: /collections: \[(?<insertion>)/,
+          separator: '',
+          template: '{{pascalCase name}}, ',
+        })
+      }
+
+      return actions
+    },
   })
 }
